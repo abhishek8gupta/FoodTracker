811526a2174461169d859c6216a0959b78a3a754
abhigup4 <abhigup4@cisco.com>
2016-03-18 22:36:11 -0700

    first commit

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,9 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Abhishek Gupta on 2/23/16.
+//  Copyright © 2016 Abhishek Gupta. All rights reserved.
+//
+
+import Foundation
---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Abhishek Gupta on 2/14/16.
+//  Copyright © 2016 Abhishek Gupta. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
